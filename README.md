bace6e3ef9a5ad710a1b768589eb1ac97cc90773
raphaelantoniocampos <raphael01ac@gmail.com>
2023-05-26 10:45:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+Pirulla Bot
+
+This project tracks the average length of videos on the Pirulla YouTube channel. It uses the Pytube library to access YouTube data and Tweepy library to post updates to Twitter when there are changes in the average length.
+
+Getting Started
+To get started, you will need to install the following dependencies:
+
+Python 3
+Tweepy
+Matplotlib
+Pytube
+
+Once you have installed the dependencies, you can clone the repository and run the following command to start the tracker:
+
+python3 main.py
+
+Usage
+The tracker will run indefinitely, checking for updates to the Pirulla YouTube channel every 5-15 minutes. When there are changes in the average length, the tracker will post an update to Twitter.
+
+Contributing
+If you would like to contribute to this project, please feel free to open an issue or pull request.
+
+Authors
+Raphael Campos - https://github.com/raphaelantoniocampos
---
+++ data_generator.py
@@ -0,0 +1,37 @@
+import json
+from pytube import Channel
+
+
+def generate_channel_data(channel):
+    """
+    Generates all data for a channel.
+
+    Args:
+        channel: A channel object.
+
+    Returns:
+        A list of dictionaries, where each dictionary contains the date and average time for a list of videos.
+    """
+    videos = list(channel.videos)
+    averages = []
+    for average_num in range(len(videos)):
+        average_data = {}
+        average_sum = 0
+        for i, video in enumerate(videos[::-1]):
+            average_sum += video.length
+            if i >= average_num:
+                average_data["Date"] = str(video.publish_date)
+                average_time = average_sum / (i + 1)
+                average_data["Average"] = average_time
+                print(f"{i + 1}/{len(videos)}: {average_data}")
+                break
+        averages.append(average_data)
+
+    filename = "channel_data.json"
+    with open(filename, "w") as f:
+        json.dump(averages, f)
+
+
+channel_url = "https://www.youtube.com/channel/UCdGpd0gNn38UKwoncZd9rmA"
+channel = Channel(channel_url)
+generate_channel_data(channel)
